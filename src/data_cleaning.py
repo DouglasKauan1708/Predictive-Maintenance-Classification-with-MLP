834119f70f99f74ea21a65a36e98417446d16de1
Douglas Kauan Dutra de Oliveira <douglaskauandutradeoliveira@gmail.com>
2026-05-01 12:51:44 -0300

    Change 3

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -18,8 +18,7 @@ def data_cleaning(df):
                 'Tool Wear Failure': 4,
                 'Random Failures': 5}
     clean_df['Failure Type'] = clean_df['Failure Type'].map(failure_map)
-    clean_df = pd.get_dummies(clean_df, columns=['Type'], drop_first=True)
-    clean_df = df.drop(columns=["Type_L", "Type_M"]) # Unrelevant columns
+    clean_df = clean_df.drop(columns=["Type"]) # Unrelevant columns
 
     # Separating our columns
     X = clean_df.drop(columns=["Failure Type"])
